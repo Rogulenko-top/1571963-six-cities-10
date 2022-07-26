6dff332385d45c806d1d3c6079f13c117ab37820
Anton Rogulenko <rogulenkoaa@gmail.com>
2022-07-26 22:19:01 +0500

    ÑВÐыполнено задание - Обратная стор4.13. Обратная сторона реальности

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -1,11 +1,13 @@
 import {Fragment} from 'react';
-import Cards from '../../components/cards/cards';
+import OffersList from '../../components/offers-list/offers-list';
+import Offer from '../../types/offer';
 
 type MainProps = {
   offersCount: number;
+  offers: Offer[];
 }
 
-function Main({ offersCount }: MainProps): JSX.Element{
+function Main({ offersCount, offers }: MainProps): JSX.Element{
   return(
     <Fragment>
       <div style={{ display: 'none' }}>
@@ -80,35 +82,7 @@ function Main({ offersCount }: MainProps): JSX.Element{
               </ul>
             </section>
           </div>
-          <div className="cities">
-            <div className="cities__places-container container">
-              <section className="cities__places places">
-                <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">312 places to stay in Amsterdam</b>
-                <form className="places__sorting" action="#" method="get">
-                  <span className="places__sorting-caption">Sort by </span>
-                  <span className="places__sorting-type" tabIndex={0}>
-                   Popular
-                    <svg className="places__sorting-arrow" width='7' height='4'>
-                      <use xlinkHref="#icon-arrow-select"></use>
-                    </svg>
-                  </span>
-                  <ul className="places__options places__options--custom places__options--opened">
-                    <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                    <li className="places__option" tabIndex={0}>Price: low to high</li>
-                    <li className="places__option" tabIndex={0}>Price: high to low</li>
-                    <li className="places__option" tabIndex={0}>Top rated first</li>
-                  </ul>
-                </form>
-                <div className="cities__places-list places__list tabs__content">
-                  {Array.from({length: offersCount},() => <Cards/>)}
-                </div>
-              </section>
-              <div className="cities__right-section">
-                <section className="cities__map map"></section>
-              </div>
-            </div>
-          </div>
+          <OffersList offers = {offers} />
         </main>
       </div>
     </Fragment>
